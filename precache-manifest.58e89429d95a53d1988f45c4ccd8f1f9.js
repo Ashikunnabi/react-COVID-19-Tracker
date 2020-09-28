9c2878f78bdbc0a4eced9dbadcc517568339a07a
Md. Ashikun Nabi <ashikunnabituhin@gmail.com>
2020-09-28 21:21:31 +0600

    Updates

---
+++ precache-manifest.58e89429d95a53d1988f45c4ccd8f1f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2ae10e453111c13a64f7d7f830f61d",
+    "url": "/react-COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "16943bb911de5d30595b",
+    "url": "/react-COVID-19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "95131eef92fbfaece379",
+    "url": "/react-COVID-19-Tracker/static/css/main.575429ae.chunk.css"
+  },
+  {
+    "revision": "16943bb911de5d30595b",
+    "url": "/react-COVID-19-Tracker/static/js/2.fec18a35.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-COVID-19-Tracker/static/js/2.fec18a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95131eef92fbfaece379",
+    "url": "/react-COVID-19-Tracker/static/js/main.1c525d76.chunk.js"
+  },
+  {
+    "revision": "40ffbd8f2ac3eaf7aa0b",
+    "url": "/react-COVID-19-Tracker/static/js/runtime-main.49be9366.js"
+  }
+]);
